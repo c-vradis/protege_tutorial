983f07e9de12e121bc2e311fe33259c2dde9d825
Christoforos Vradis <cvradis@hua.gr>
2025-03-04 22:45:52 +0200

    Αναδιοργάνωση αρχείων, νέες εκδόσεις για το 2025 κ.α.

---
+++ scripts/marp2pdf.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+npx marp --allow-local-files --pdf source_files/SW_LOD_Protege_Tutorial.md -o 2025/SW_LOD_Protege_Tutorial.pdf
+npx marp --allow-local-files --pdf source_files/SW_LOD_Presentation.md -o 2025/SW_LOD_Presentation.pdf
--- scripts/md2pdf.sh
+++ scripts/md2pdf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-declare -a files=("01_Εισαγωγή" "02_About_Harokopio" "03_Tutorial" "README")
+declare -a files=("SW_LOD_Presentation" "SW_LOD_Protege_Tutorial" "README")
 
 for ((i=0;i<${#files[@]};i++)); do
-pandoc --pdf-engine=xelatex --toc=false --dpi=150 --columns=120 -V mainfont="HelveticaNeue" -V monofont="AndaleMono" ${files[i]}.md -o ${files[i]}.pdf
+pandoc --pdf-engine=xelatex --toc=false --dpi=150 --columns=120 -V mainfont="HelveticaNeue" -V monofont="AndaleMono" source_files/${files[i]}.md -o 2025/${files[i]}.pdf
 done
